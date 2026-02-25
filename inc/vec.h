d25abacec2d78de70453a683b5b53eacee498fd3
ActuallyMadhav <madhavra.2004@gmail.com>
2026-02-26 00:23:00 +0800

    changed scale and normalize functions to modify values in place

--- inc/vec.h
+++ inc/vec.h
@@ -12,15 +12,15 @@ typedef struct {float x, y, z, w;} vec4;
 // arithmetic operations
 vec2 vec2_add(vec2 a, vec2 b);
 vec2 vec2_subtract(vec2 a, vec2 b);
-vec2 vec2_scale(vec2 v, float k);
+void vec2_scale(vec2* v, float k);
 
 vec3 vec3_add(vec3 a, vec3 b);
 vec3 vec3_subtract(vec3 a, vec3 b);
-vec3 vec3_scale(vec3 v, float k);
+void vec3_scale(vec3* v, float k);
 
 vec4 vec4_add(vec4 a, vec4 b);
 vec4 vec4_subtract(vec4 a, vec4 b);
-vec4 vec4_scale(vec4 v, float k);
+void vec4_scale(vec4* v, float k);
 
 // dot and cross products
 float vec2_dot(vec2 a, vec2 b);
@@ -34,9 +34,15 @@ float vec2_length(vec2 v);
 float vec3_length(vec3 v);
 float vec4_length(vec4 v);
 
-vec2 vec2_normalize(vec2 v);
-vec3 vec3_normalize(vec3 v);
-vec4 vec4_normalize(vec4 v);
+// in-place modification
+void vec2_normalize(vec2* v);
+void vec3_normalize(vec3* v);
+void vec4_normalize(vec4* v);
+
+// new vec returned
+vec2 vec2_normalized(vec2 v);
+vec3 vec3_normalized(vec3 v);
+vec3 vec4_normalized(vec4 v);
 
 // prints for debugging
 void vec2_print(vec2 v);
--- src/vec.c
+++ src/vec.c
@@ -8,3 +8,15 @@ vec2 vec2_add(vec2 a, vec2 b){
     result.y = a.y + b.y;
     return result;
 }
+
+vec2 vec2_subtract(vec2 a, vec2 b){
+    vec2 result;
+    result.x = a.x - b.x;
+    result.y = a.y - b.y;
+    return result;
+}
+
+void vec2_scale(vec2* v, float k){
+    v->x *= k;
+    v->y *= k;
+}
