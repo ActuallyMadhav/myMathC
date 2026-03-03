e9b394e8b1affeb561e8d4c504cf5e9743e75eb0
ActuallyMadhav <madhavra.2004@gmail.com>
2026-03-03 23:32:20 +0800

    created matrix files

---
+++ inc/mat.h
@@ -0,0 +1,14 @@
+#ifndef MAT_H
+#define MAT_H
+
+#include <math.h>
+#include <stdio.h>
+
+typedef struct{} mat2;
+typedef struct{} mat3;
+typedef struct{} mat4;
+
+mat2 mat2_add(mat2 a, mat2 b);
+
+
+#endif
