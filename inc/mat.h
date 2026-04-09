a55fcd2b879a43ba028fa5fba26c403aa384f6b7
ActuallyMadhav <madhavra.2004@gmail.com>
2026-04-10 00:14:19 +0800

    added some operations but feeling sleepy now

--- inc/mat.h
+++ inc/mat.h
@@ -4,11 +4,17 @@
 #include <math.h>
 #include <stdio.h>
 
-typedef struct{} mat2;
-typedef struct{} mat3;
-typedef struct{} mat4;
+// mat structs, should i use pointers?
+typedef struct{float m[4];} mat2;
+typedef struct{float m[9];} mat3;
+typedef struct{float m[16]} mat4;
 
+// mat2 operations
 mat2 mat2_add(mat2 a, mat2 b);
+mat2 mat2_subtract(mat2 a, mat2 b);
+mat2 mat2_scale_new(mat2 a, int k);
+void mat2_scale(mat2* a, int k);
+mat2 mat2_transpose(mat2 a);
 
 
 #endif
