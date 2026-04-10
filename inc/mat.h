8f74881bc7e585e76478f7e6743619b98009f43f
ActuallyMadhav <madhavra.2004@gmail.com>
2026-04-10 19:59:11 +0800

    added prototypes and mat2_add

--- inc/mat.h
+++ inc/mat.h
@@ -7,14 +7,46 @@
 // mat structs, should i use pointers?
 typedef struct{float m[4];} mat2;
 typedef struct{float m[9];} mat3;
-typedef struct{float m[16]} mat4;
+typedef struct{float m[16];} mat4;
+
+// identity matrices
+const mat2 IDENTITY_2x2 = {
+    1.0f, 0.0f,
+    0.0f, 1.0f
+};
+
+const mat3 IDENTITY_3x3 = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+};
+
+const mat4 IDENTITY_4x4 = {
+    1.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 1.0f
+};
 
 // mat2 operations
 mat2 mat2_add(mat2 a, mat2 b);
 mat2 mat2_subtract(mat2 a, mat2 b);
-mat2 mat2_scale_new(mat2 a, int k);
-void mat2_scale(mat2* a, int k);
+mat2 mat2_scale_new(mat2 a, float k);
+void mat2_scale(mat2* a, float k);
 mat2 mat2_transpose(mat2 a);
 
+// mat3 operations
+mat3 mat3_add(mat3 a, mat3 b);
+mat3 mat3_subtract(mat3 a, mat3 b);
+mat3 mat3_scale_new(mat3 a, float k);
+void mat3_scale(mat3* a, float k);
+mat3 mat3_transpose(mat3 a);
+
+// mat4 operations
+mat4 mat4_add(mat4 a, mat4 b);
+mat4 mat4_subtract(mat4 a, mat4 b);
+mat4 mat4_scale_new(mat4 a, float k);
+void mat4_scale(mat4 a, float k);
+mat4 mat4_transpose(mat4 a);
 
 #endif
--- src/mat.c
+++ src/mat.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 #include "mat.h"
+
+// mat2 operations
+mat2 mat2_add(mat2 a, mat2 b){
+    mat2 res = {
+        a.m[0] + b.m[0], a.m[1] + b.m[1],
+        a.m[2] + b.m[2], a.m[3] + b.m[3]
+    };
+    return res;
+}
